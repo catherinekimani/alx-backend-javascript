31ac731192a186a61c78d95e359014dd2164784e
catherinekimani <catherinekimani882@gmail.com>
2024-01-05 03:11:24 +0300

    Update Building class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
