3c3b2c51af80521929be3567fd0141a1d3d82ed7
catherinekimani <catherinekimani882@gmail.com>
2024-03-02 02:00:45 +0300

    Feat: add API endpoints

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+const port = 7865;
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+	response.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (request, response) => {
+	response.send(`Payment methods for cart ${request.params.id}`)
+});
+
+app.get('/available_payments', (request, response) => {
+	response.set("Content-Type", "application/json");
+	const PaymentOptions = {
+    payment_methods: {
+    credit_cards: true,
+    paypal: false
+  }
+}
+	response.send(PaymentOptions);
+});
+
+app.post('/login', (request, response) => {
+	const user = request.body.user || request.body.userName;
+	if (user) {
+		response.send(`Welcome ${user}`);
+	} else {
+		response.status(404).send();
+	}
+});
+
+app.listen(7865, () => {
+	console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
