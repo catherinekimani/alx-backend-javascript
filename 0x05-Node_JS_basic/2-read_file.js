6bf3c6511430603e97797432810e556bf6ff191a
catherinekimani <catherinekimani882@gmail.com>
2024-03-01 20:40:31 +0300

    feat: Implement countStudents func

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+
+  const data = fs.readFileSync(path, 'utf8');
+  const lines = data.split('\n');
+  const hashTable = {};
+  let students = -1;
+  for (const line of lines) {
+    if (line.trim() !== '') {
+      const columns = line.split(',');
+      const field = columns[3];
+      const firstName = columns[0];
+      if (students >= 0) {
+        if (!Object.hasOwnProperty.call(hashTable, field)) {
+          hashTable[field] = [];
+        }
+        hashTable[field] = [...hashTable[field], firstName];
+      }
+      students += 1;
+    }
+  }
+  console.log(`Number of students: ${students}`);
+  for (const key in hashTable) {
+    if (Object.hasOwnProperty.call(hashTable, key)) {
+      console.log(`Number of students in ${key}: ${hashTable[key].length}. List: ${hashTable[key].join(', ')}`);
+    }
+  }
+}
+module.exports = countStudents;
