9b9effd4bb3d39ca1236535962b2bac8b5d43af4
catherinekimani <catherinekimani882@gmail.com>
2024-03-02 07:38:40 +0300

    Add test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,5 +7,7 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3)
+    assert.strictEqual(calculateNumber(-1.7, -2), -4)
   });
 });
